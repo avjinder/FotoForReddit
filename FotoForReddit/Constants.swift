549237bad9c810734d837524f2abe95d69a482bc
Avjinder Singh Sekhon <avisekhon@gmail.com>
2017-04-13 21:30:37 +0530

    Moved some files around

---
+++ FotoForReddit/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  UserDefaultsConstants.swift
+//  FotoForReddit
+//
+//  Created by Avjinder Singh Sekhon on 3/30/17.
+//  Copyright © 2017 Avjinder Singh Sekhon. All rights reserved.
+//
+
+import Foundation
+
+public struct Constants{
+    static let UserSubredditsDefaults = "UserSubredditDefaults"
+    static let WalkthroughViewControllerID = "WalkthroughViewControllerID"
+}
+
